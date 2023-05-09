1dd731f6991ce1ef4136744cae88a4025b00a7cc
giang <giang.nth1902@gmail.com>
2023-05-09 11:22:17 +0700

    generate map

--- core/src/com/mygdx/soulknight/Game.java
+++ core/src/com/mygdx/soulknight/Game.java
@@ -1,31 +1,57 @@
 package com.mygdx.soulknight;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private OrthographicCamera camera;
+	private TiledMap tiledMap;
+	private OrthogonalTiledMapRenderer mapRenderer;
+	private TiledMapTileLayer collisionLayer;
+	private float elapsedSeconds = 0;
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		// Set up the camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		// Load the map
+		tiledMap = new TmxMapLoader().load("maps.tmx");
+		mapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		collisionLayer = (TiledMapTileLayer) tiledMap.getLayers().get("Collisions");
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		// Clear the screen
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		elapsedSeconds += Gdx.graphics.getDeltaTime();
+
+		// Render the map and the character
+		mapRenderer.setView(camera);
+		mapRenderer.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		// Dispose of resources
+		tiledMap.dispose();
+		mapRenderer.dispose();
 	}
 }
