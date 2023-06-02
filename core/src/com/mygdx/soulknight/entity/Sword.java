fcdaa0d62b153f0adc06537a000a3f41d09a2654
huydsai02 <huydsai02@gmail.com>
2023-06-03 00:00:46 +0700

    Create Sword.java

---
+++ core/src/com/mygdx/soulknight/entity/Sword.java
@@ -0,0 +1,25 @@
+package com.mygdx.soulknight.entity;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Sword extends Weapon {
+
+    public Sword(SimpleCharacter owner, String texturePath) {
+        super(owner, texturePath);
+    }
+
+    public Sword(SimpleCharacter owner, String texturePath, float intervalSeconds) {
+        super(owner, texturePath, intervalSeconds);
+    }
+
+    @Override
+    public void attack(Vector2 direction) {
+
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+
+    }
+}
