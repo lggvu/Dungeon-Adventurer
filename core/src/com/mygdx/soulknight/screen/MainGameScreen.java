a5db33e8215816c5b7c2024ce6099551ece5c6b1
Vu Hoang <61404697+lggvu@users.noreply.github.com>
2023-06-23 14:24:45 +0700

    update assassin char + dying effects

--- core/src/com/mygdx/soulknight/screen/MainGameScreen.java
+++ core/src/com/mygdx/soulknight/screen/MainGameScreen.java
@@ -44,7 +44,7 @@ public class MainGameScreen extends ScreenAdapter {
 
     public MainGameScreen(SoulKnight game) {
         this.game = game;
-        player = new Player("jungler", null);
+        player = new Player("assassin", null);
         map = new WorldMap("split_map/tmx/map_2.tmx", player);
         minimap = new Minimap(map.getTiledMap(), player);
         player.setMap(map);
