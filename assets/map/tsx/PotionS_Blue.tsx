aff64e39cf230a854c3dfbd95f335254285d60bf
giang <giang.nth1902@gmail.com>
2023-07-02 23:54:29 +0700

    complete map3

---
+++ assets/map/tsx/PotionS_Blue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="PotionS_Blue" tilewidth="16" tileheight="16" tilecount="7" columns="7">
+ <image source="../img/Mini_Dungeon_v1.0/Objects/PotionS_Blue.png" width="112" height="16"/>
+</tileset>
